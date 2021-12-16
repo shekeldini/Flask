900ee0416b62a6cd835ab537cf5bc465c103710d
Исаян Денис Валерьевич <idv@akiac.ru>
2021-12-16 15:55:12 +0700

    Update task_description.js

--- static/js/task_description.js
+++ static/js/task_description.js
@@ -131,7 +131,8 @@ fetch('/api/task_description/get_task_numbers/' + id_oo + '/' + parallel + '/' +
         });
 };
 
-report_select.onchange = function(){
+
+task_select.onchange = function(){
 task_number = task_select.value;
 fetch('/api/task_description/get_reports/' + task_number).then(function(response){
                 response.json().then(function(data) {
